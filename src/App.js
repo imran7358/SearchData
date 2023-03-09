ef697b80fd446a313c71a7a4f31e62ceadcf7968
Mohammad Imran <mohammadimran@Mohammads-MacBook-Pro.local>
2023-03-09 17:01:12 +0530

    Data Search
    Signed-off-by: Mohammad Imran <mohammadimran@Mohammads-MacBook-Pro.local>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React, {useEffect, useState} from 'react';
 
 function App() {
+const [data, setData] = useState([])
+const [input, setInput] = useState('')
+  useEffect(()=>{
+    axios.get("https://jsonplaceholder.typicode.com/posts").then(({data})=>{
+      console.log(data)
+      const sortArray = data.sort((a,b)=>{
+        return a.title > b.title
+      })
+      console.log("sortArray", sortArray)
+      setData(sortArray)
+    }).catch((error)=>{
+      console.log(error)
+    })
+  },[])
+  useEffect(()=>{
+    console.log(input)
+  },[data, input])
+  const filterData = data.filter((item)=>{
+    if (input === ''){
+      return item
+    } 
+    else {
+      return item.title.toLowerCase().includes(input)
+
+    }
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Data</h1>
+      <input type="text" placeholder='Search Data' onChange={(e)=> setInput(e.target.value)}/>
+      {
+        filterData.length ? filterData.map((item,i)=>{
+          return <ul key={i}>
+            <li>{item.title}</li>
+          </ul>
+        }) :null
+      }
     </div>
   );
 }
